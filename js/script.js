343772924bf13adc0fad41dbd6d582625134d976
toledoantonio <antonioramont7@gmail.com>
2022-05-15 17:33:31 -0300

    Se agrego contenido a Preguntas Frecuentes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
 const search = document.querySelector(".search-box input"),
-      images = document.querySelectorAll(".image-box");
+    images = document.querySelectorAll(".image-box");
 
-search.addEventListener("keyup", e =>{
-    if(e.key == "Enter"){
+search.addEventListener("keyup", e => {
+    if (e.key == "Enter") {
         let searcValue = search.value,
             value = searcValue.toLowerCase();
-            images.forEach(image =>{
-                if(value === image.dataset.name){ //matching value with getting attribute of images
-                    return image.style.display = "block";
-                }
-                image.style.display = "none";
-         });
+        images.forEach(image => {
+            if (value === image.dataset.name) { //matching value with getting attribute of images
+                return image.style.display = "block";
+            }
+            image.style.display = "none";
+        });
     }
 });
 
-search.addEventListener("keyup", () =>{
-    if(search.value != "") return;
+search.addEventListener("keyup", () => {
+    if (search.value != "") return;
 
-    images.forEach(image =>{
+    images.forEach(image => {
         image.style.display = "block";
+    })
+})
+
+const questions = document.querySelectorAll('.question-answer');
+
+questions.forEach(function (question) {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener("click", function () {
+        questions.forEach(function (item) {
+            if (item !== question) {
+                item.classList.remove("show-text");
+            }
+        })
+        question.classList.toggle("show-text");
     })
 })
